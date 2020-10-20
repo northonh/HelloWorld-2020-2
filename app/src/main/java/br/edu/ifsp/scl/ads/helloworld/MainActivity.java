d4189dbf6f9ab4c938a477d103b0f842f0696bff
northonh <nobile.pedro@gmail.com>
2020-10-20 20:30:52 -0300

    Tratamento de eventos 4: Resumo das formas de tratamento de eventos de clique em objetos Button.

--- app/src/main/java/br/edu/ifsp/scl/ads/helloworld/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/helloworld/MainActivity.java
@@ -1,26 +1,49 @@
 package br.edu.ifsp.scl.ads.helloworld;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    // referência para o objeto Button definido no leiaute
-    private Button cliqueAquiBt;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    // referência para os objetos Button definidos no leiaute
+    private Button zeroBt, umBt, doisBt, limparBt;
+    private TextView visorTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getString(R.string.zero);
 
         // recuperando e atribuindo referência para objeto Button inflado a partir do leiaute
-        cliqueAquiBt = findViewById(R.id.clique_aqui_bt);
+        visorTv = findViewById(R.id.visorTv);
+        zeroBt = findViewById(R.id.zeroBt);
+        umBt = findViewById(R.id.umBt);
+        limparBt = findViewById(R.id.limparBt);
 
         /* setando listener de tratamento de eventos de clique no botão como uma função lambda */
-        cliqueAquiBt.setOnClickListener(
-                v -> Toast.makeText(getApplicationContext(), "Você clicou aqui (lambda)", Toast.LENGTH_SHORT).show()
+        zeroBt.setOnClickListener(this);
+        umBt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                visorTv.setText(getString(R.string.um));
+            }
+        });
+        limparBt.setOnClickListener(
+                v -> visorTv.setText("")
         );
+    }
+
+    @Override
+    public void onClick(View v) {
+        visorTv.setText(getString(R.string.zero));
+    }
+
+    public void onClickDois(View view) {
+        visorTv.setText(getString(R.string.dois));
     }
 }
