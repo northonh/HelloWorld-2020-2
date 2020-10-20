99de23ac222b0712dc7dc2bec61e0e2ec50881fd
northonh <nobile.pedro@gmail.com>
2020-10-20 14:39:02 -0300

    Tratamento de eventos 3: Atendendo a uma solicitação de vocês. Usando uma função lambda para tratar o evento de clique no objeto Button.

--- app/src/main/java/br/edu/ifsp/scl/ads/helloworld/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/helloworld/MainActivity.java
@@ -1,9 +1,9 @@
 package br.edu.ifsp.scl.ads.helloworld;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,13 +18,9 @@ public class MainActivity extends AppCompatActivity {
         // recuperando e atribuindo referência para objeto Button inflado a partir do leiaute
         cliqueAquiBt = findViewById(R.id.clique_aqui_bt);
 
-        /* setando listener de tratamento de eventos de clique no botão como um objeto único de
-        uma classe anônima */
-        cliqueAquiBt.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Você clicou aqui (anônimo)", Toast.LENGTH_SHORT).show();
-            }
-        });
+        /* setando listener de tratamento de eventos de clique no botão como uma função lambda */
+        cliqueAquiBt.setOnClickListener(
+                v -> Toast.makeText(getApplicationContext(), "Você clicou aqui (lambda)", Toast.LENGTH_SHORT).show()
+        );
     }
 }
