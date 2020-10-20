0bc3a8a670615618996082f05940bfc43591cb13
northonh <nobile.pedro@gmail.com>
2020-10-20 14:29:37 -0300

    Tratamento de eventos 1: Tratando evento de clique no objeto Button usando implementação da interface OnClickListener.

--- app/src/main/java/br/edu/ifsp/scl/ads/helloworld/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/helloworld/MainActivity.java
@@ -1,14 +1,30 @@
 package br.edu.ifsp.scl.ads.helloworld;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+    // referência para o objeto Button definido no leiaute
+    private Button cliqueAquiBt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // recuperando e atribuindo referência para objeto Button inflado a partir do leiaute
+        cliqueAquiBt = findViewById(R.id.clique_aqui_bt);
+
+        // setando listener de tratamento de eventos de clique no botão
+        cliqueAquiBt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // tratando evento de clique no botão mostrando uma mensagem curta para o usuário
+        Toast.makeText(this, "Você clicou aqui", Toast.LENGTH_SHORT).show();
     }
 }
