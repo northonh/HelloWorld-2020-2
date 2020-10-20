2dd2942e9eefbd09976b680ed6a065fc203d4672
northonh <nobile.pedro@gmail.com>
2020-10-20 14:33:57 -0300

    Tratamento de eventos 2: Tratando evento de clique no objeto Button usando um objeto único de classe anônima que implementa a interface OnClickListener.

--- app/src/main/java/br/edu/ifsp/scl/ads/helloworld/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/helloworld/MainActivity.java
@@ -6,7 +6,7 @@ import android.widget.Button;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     // referência para o objeto Button definido no leiaute
     private Button cliqueAquiBt;
 
@@ -18,13 +18,13 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         // recuperando e atribuindo referência para objeto Button inflado a partir do leiaute
         cliqueAquiBt = findViewById(R.id.clique_aqui_bt);
 
-        // setando listener de tratamento de eventos de clique no botão
-        cliqueAquiBt.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        // tratando evento de clique no botão mostrando uma mensagem curta para o usuário
-        Toast.makeText(this, "Você clicou aqui", Toast.LENGTH_SHORT).show();
+        /* setando listener de tratamento de eventos de clique no botão como um objeto único de
+        uma classe anônima */
+        cliqueAquiBt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Você clicou aqui (anônimo)", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
