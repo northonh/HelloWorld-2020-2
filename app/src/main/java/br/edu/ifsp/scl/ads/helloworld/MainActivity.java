99a700eb153eb740f33f269b68395ed39a06f991
northonh <nobile.pedro@gmail.com>
2020-10-20 21:23:49 -0300

    Tratando eventos em um único método. Usando a classe Log para instrumentação de logs.

--- app/src/main/java/br/edu/ifsp/scl/ads/helloworld/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/helloworld/MainActivity.java
@@ -1,49 +1,39 @@
 package br.edu.ifsp.scl.ads.helloworld;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     // referência para os objetos Button definidos no leiaute
-    private Button zeroBt, umBt, doisBt, limparBt;
     private TextView visorTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getString(R.string.zero);
 
-        // recuperando e atribuindo referência para objeto Button inflado a partir do leiaute
         visorTv = findViewById(R.id.visorTv);
-        zeroBt = findViewById(R.id.zeroBt);
-        umBt = findViewById(R.id.umBt);
-        limparBt = findViewById(R.id.limparBt);
+    }
 
-        /* setando listener de tratamento de eventos de clique no botão como uma função lambda */
-        zeroBt.setOnClickListener(this);
-        umBt.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.zeroBt:
+                visorTv.setText(getString(R.string.zero));
+                Log.v(getString(R.string.app_name), getString(R.string.zero));
+                break;
+            case R.id.umBt:
                 visorTv.setText(getString(R.string.um));
-            }
-        });
-        limparBt.setOnClickListener(
-                v -> visorTv.setText("")
-        );
-    }
-
-    @Override
-    public void onClick(View v) {
-        visorTv.setText(getString(R.string.zero));
-    }
-
-    public void onClickDois(View view) {
-        visorTv.setText(getString(R.string.dois));
+                break;
+            case R.id.doisBt:
+                visorTv.setText(getString(R.string.dois));
+                break;
+            case R.id.limparBt:
+                visorTv.setText("");
+                break;
+        }
     }
 }
