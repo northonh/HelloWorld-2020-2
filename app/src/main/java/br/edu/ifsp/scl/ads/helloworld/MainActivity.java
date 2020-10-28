7d3c0f9dba91cc5ea6609dbe6df7872807faac2f
northonh <nobile.pedro@gmail.com>
2020-10-27 21:46:16 -0300

    Salvando e restaurando dados de instância.

--- app/src/main/java/br/edu/ifsp/scl/ads/helloworld/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/helloworld/MainActivity.java
@@ -1,14 +1,19 @@
 package br.edu.ifsp.scl.ads.helloworld;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    // Constante para salvamento/restauração de variáveis de instância
+    private final String VALOR_VISOR_TV = "VALOR_VISOR_TV";
+
     // referência para os objetos Button definidos no leiaute
     private TextView visorTv;
 
@@ -19,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         visorTv = findViewById(R.id.visorTv);
+        if (savedInstanceState != null) {
+            visorTv.setText(savedInstanceState.getString(VALOR_VISOR_TV, ""));
+        }
     }
 
     @Override
@@ -49,6 +57,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.v(getString(R.string.app_name), "onDestroy executado - finalizado ciclo completo");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.v(getString(R.string.app_name), "onSaveInstanceState executado - salvando dados de instância");
+        outState.putString(VALOR_VISOR_TV, visorTv.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.v(getString(R.string.app_name), "onRestoreInstanceState executado - restaurando dados de instância");
+        //visorTv.setText(savedInstanceState.getString(VALOR_VISOR_TV, ""));
     }
 
     public void onClick(View view) {
